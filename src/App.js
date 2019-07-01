e752122a0e90840df4e7c7c2376eab48f67a0ff0
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-07-01 21:38:48 +0100

    feat: setup the basic ui template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from '@material-ui/core/Container';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import { blue, grey } from '@material-ui/core/colors';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 
+const theme = createMuiTheme({
+	palette: {
+		primary: blue,
+		secondary: grey,
+		background: {
+			default: '#ffffff'
+		}
+	}
+});
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex'
+	},
+	toolbar: theme.mixins.toolbar,
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3)
+	}
+}));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const classes = useStyles();
+
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<div className={'App ' + classes.root}>
+				<Container maxWidth="lg">
+					<div className={classes.toolbar} />
+					<main className={classes.content}>My App</main>
+					<div className={classes.toolbar} />
+				</Container>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
