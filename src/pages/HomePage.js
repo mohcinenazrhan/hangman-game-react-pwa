18d053922a4ac30587cbe48dcfdb355b5e87c45b
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-07-19 22:22:05 +0100

    feat: add content to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Button, Link } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	link: {
+		margin: theme.spacing(1)
+	},
+	button: {
+		margin: theme.spacing(0.5),
+		padding: 0,
+		fontWeight: 'bold',
+		[theme.breakpoints.up('sm')]: {
+			margin: theme.spacing(1),
+			padding: '6px 16px'
+		}
+	},
+	marginTopBottom: {
+		marginTop: theme.spacing(5),
+		marginBottom: theme.spacing(5)
+	}
+}));
 
 const HomePage = () => {
+	const classes = useStyles();
+
 	return (
-		<Typography variant="h4" component="h1">
-			Home page
-		</Typography>
+		<React.Fragment>
+			<Typography variant="h4" component="h1">
+				Hangman Game
+			</Typography>
+			<Typography variant="body1" gutterBottom className={classes.marginTopBottom}>
+				Hangman is a paper and pencil guessing game for two or more players. One player thinks of a word, phrase
+				or sentence and the other(s) tries to guess it by suggesting letters or numbers, within a certain number
+				of guesses.
+				<Link
+					href="https://en.wikipedia.org/wiki/Hangman_(game)"
+					target="_blank"
+					rel="noreferrer"
+					className={classes.link}
+				>
+					See more
+				</Link>
+			</Typography>
+		</React.Fragment>
 	);
 };
 
