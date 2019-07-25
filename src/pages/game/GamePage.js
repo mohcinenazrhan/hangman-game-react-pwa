770320bb78db018278d2ad3532f748bf00bad6da
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-07-25 16:14:36 +0100

    feat: add Dexie to save session data in local db

--- src/pages/game/GamePage.js
+++ src/pages/game/GamePage.js
@@ -15,6 +15,7 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Game from './components/Game';
+import Dexie from 'dexie';
 
 const useStyles = makeStyles((theme) => ({
 	formControl: {
@@ -53,6 +54,12 @@ const GamePage = () => {
 
 	// State for the dependencies if ready
 	const [ isReady, setIsReady ] = React.useState(false);
+
+	// Define the database
+	const db = new Dexie('sessionsDb');
+	db.version(1).stores({
+		sessions: '++id,date,ended,score'
+	});
 
 	React.useEffect(
 		() => {
