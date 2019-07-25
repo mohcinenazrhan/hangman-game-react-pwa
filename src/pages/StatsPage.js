ee3c208706525735a490315e6ff42a475bd299ab
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-07-25 19:45:36 +0100

    feat: show last 5 sessions data from local Db

--- src/pages/StatsPage.js
+++ src/pages/StatsPage.js
@@ -1,11 +1,149 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Paper, Table, TableRow, TableHead, TableCell, TableBody, Divider } from '@material-ui/core';
+import clsx from 'clsx';
+import Dexie from 'dexie';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: '100%'
+	},
+	paper: {
+		marginTop: theme.spacing(3),
+		width: '100%',
+		overflowX: 'auto',
+		marginBottom: theme.spacing(2)
+	},
+	tableCell: {
+		padding: '5px'
+	},
+	tableCellNoPadding: {
+		padding: '0px !important'
+	},
+	letterFound: {
+		color: '#27ae60'
+	},
+	letterShow: {
+		color: '#c0392b'
+	},
+	succeed: {
+		borderLeft: '6px solid #2ecc71'
+	},
+	failed: {
+		borderLeft: '6px solid #e74c3c'
+	},
+	statsContainer: {
+		marginTop: theme.spacing(3),
+		marginBottom: theme.spacing(3),
+		paddingTop: theme.spacing(3),
+		paddingBottom: theme.spacing(3)
+	}
+}));
 
 const StatesPage = () => {
+	const classes = useStyles();
+	const [ stats, setStats ] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const dbOpened = await new Dexie('sessionsDb').open();
+				if (dbOpened) {
+					dbOpened._allTables.sessions.orderBy('date').reverse().limit(5).toArray((rows) => setStats(rows));
+					return dbOpened.close();
+				}
+			} catch (error) {
+				console.log(error.message);
+			}
+		}
+		fetchData();
+	}, []);
+
 	return (
-		<Typography variant="h4" component="h1">
-			My states
-		</Typography>
+		<div className={classes.root}>
+			{stats.length > 0 && (
+				<React.Fragment>
+					<Typography variant="h5" component="h1">
+						My sessions stats{' '}
+					</Typography>
+					{stats.map((statsRow, index) => (
+						<React.Fragment key={index}>
+							<div className={classes.statsContainer}>
+								<Typography variant="h6" component="h2">
+									Session {statsRow.id}
+								</Typography>
+								<Typography>Started: {statsRow.date.toString()}</Typography>
+								<Typography>Score: {statsRow.id}</Typography>
+								<Typography>Difficulty: {statsRow.difficulty}</Typography>
+								<Typography>Language: {statsRow.language}</Typography>
+								<Typography>{statsRow.ended ? 'Completed' : 'Uncompleted'}</Typography>
+								<Typography>
+									{statsRow.playedWords ? statsRow.playedWords.length : 0}/{statsRow.words.length}{' '}
+									words played
+								</Typography>
+								{statsRow.playedWords &&
+								statsRow.playedWords.length > 0 && (
+									<Paper className={classes.paper}>
+										<Table className={classes.table} size="small">
+											<TableHead>
+												<TableRow>
+													<TableCell>Word</TableCell>
+													<TableCell>State</TableCell>
+													<TableCell align="right">Score</TableCell>
+													<TableCell align="right">Misses</TableCell>
+												</TableRow>
+											</TableHead>
+											<TableBody>
+												{statsRow.playedWords.map((row, index) => (
+													<TableRow key={index}>
+														<TableCell
+															className={clsx(
+																classes.tableCell,
+																row.result === 'succeed' && classes.succeed,
+																row.result === 'failed' && classes.failed
+															)}
+														>
+															{row.word}
+														</TableCell>
+														<TableCell className={classes.tableCell}>
+															{row.wordState.map((item, i) => {
+																return (
+																	<span
+																		key={i}
+																		className={clsx(
+																			item.state === 'found' &&
+																				classes.letterFound,
+																			item.state === 'show' && classes.letterShow
+																		)}
+																	>
+																		{item.letter}
+																	</span>
+																);
+															})}
+														</TableCell>
+														<TableCell className={classes.tableCell} align="right">
+															{row.score > 1 ? (
+																`${row.score} Points`
+															) : (
+																`${row.score} Point`
+															)}
+														</TableCell>
+														<TableCell className={classes.tableCell} align="right">
+															{`${row.misses}/${row.wrongGuessAllowed}`}
+														</TableCell>
+													</TableRow>
+												))}
+											</TableBody>
+										</Table>
+									</Paper>
+								)}
+							</div>
+							<Divider />
+						</React.Fragment>
+					))}
+				</React.Fragment>
+			)}
+		</div>
 	);
 };
 
