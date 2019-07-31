59fa1552fc715f3b444c5bdc95fcf8d394dd5b77
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-07-31 12:23:59 +0100

    feat: re-design and re-structer home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Typography, Button, Link } from '@material-ui/core';
+import { Typography, Button, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+	paper: {
+		padding: theme.spacing(1.5, 1),
+		margin: theme.spacing(2, 0),
+		background: '#FAFAFA',
+		textAlign: 'left',
+		display: 'flex',
+		justifyContent: 'space-between',
+		flexWrap: 'wrap'
+	},
+	paperInfoContainer: {},
+	paperActionContainer: {
+		alignSelf: 'center'
+	},
 	link: {
 		margin: theme.spacing(1)
 	},
@@ -24,30 +37,63 @@ const useStyles = makeStyles((theme) => ({
 const HomePage = ({ goToPage }) => {
 	const classes = useStyles();
 
-	function handleOnClick() {
+	function handleNewSessionClick() {
 		goToPage('game');
 	}
+
+	function handleContinueClick() {}
 
 	return (
 		<React.Fragment>
 			<Typography variant="h4" component="h1">
-				Hangman Game
+				Play and improve your English
 			</Typography>
-			<Typography variant="body1" gutterBottom className={classes.marginTopBottom}>
-				Hangman is a paper and pencil guessing game for two or more players. One player thinks of a word, phrase
-				or sentence and the other(s) tries to guess it by suggesting letters or numbers, within a certain number
-				of guesses.
-				<Link
-					href="https://en.wikipedia.org/wiki/Hangman_(game)"
-					target="_blank"
-					rel="noreferrer"
-					className={classes.link}
-				>
-					See more
-				</Link>
-			</Typography>
-			<Button variant="contained" color="primary" className={classes.button} onClick={handleOnClick}>
-				Start
+			<section className={classes.marginTopBottom}>
+				<Typography variant="h5" component="h2">
+					Pick up where you left off
+				</Typography>
+				<Paper className={classes.paper}>
+					<div className={classes.paperInfoContainer}>
+						<Typography variant="h6" component="h3">
+							Session 3
+						</Typography>
+						<Typography component="p">5 Words left to discover</Typography>
+					</div>
+					<div className={classes.paperActionContainer}>
+						<Button variant="contained" color="primary" onClick={handleContinueClick}>
+							Continue
+						</Button>
+					</div>
+				</Paper>
+				<Paper className={classes.paper}>
+					<div className={classes.paperInfoContainer}>
+						<Typography variant="h6" component="h3">
+							Session 2
+						</Typography>
+						<Typography component="p">2 Words left to discover</Typography>
+					</div>
+					<div className={classes.paperActionContainer}>
+						<Button variant="contained" color="primary" onClick={handleContinueClick}>
+							Continue
+						</Button>
+					</div>
+				</Paper>
+				<Paper className={classes.paper}>
+					<div className={classes.paperInfoContainer}>
+						<Typography variant="h6" component="h3">
+							Session 1
+						</Typography>
+						<Typography component="p">3 Words left to discover</Typography>
+					</div>
+					<div className={classes.paperActionContainer}>
+						<Button variant="contained" color="primary" onClick={handleContinueClick}>
+							Continue
+						</Button>
+					</div>
+				</Paper>
+			</section>
+			<Button variant="contained" color="primary" className={classes.button} onClick={handleNewSessionClick}>
+				New Session
 			</Button>
 		</React.Fragment>
 	);
