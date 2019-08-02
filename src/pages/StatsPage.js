c5a674f8b30bad080676f950dd9758f0d8454152
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-08-02 22:31:11 +0100

    feat: add progress bar to session stats

--- src/pages/StatsPage.js
+++ src/pages/StatsPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Divider, Button, Paper } from '@material-ui/core';
+import { makeStyles, withStyles, lighten } from '@material-ui/core/styles';
+import { Typography, Divider, Button, Paper, LinearProgress } from '@material-ui/core';
 import LocalDb from '../LocalDb';
 import SessionWordsStats from './common/SessionWordsStats';
 
@@ -44,8 +44,28 @@ const useStyles = makeStyles((theme) => ({
 	statsHeader: {
 		display: 'flex',
 		justifyContent: 'space-between'
+	},
+	progressInfoContainer: {
+		display: 'flex',
+		flexWrap: 'nowrap',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		width: '100%'
 	}
 }));
+
+const BorderLinearProgress = withStyles({
+	root: {
+		height: 5,
+		backgroundColor: lighten('#4CAF50', 0.5),
+		width: '100%',
+		borderRadius: 5
+	},
+	bar: {
+		borderRadius: 5,
+		backgroundColor: '#4CAF50'
+	}
+})(LinearProgress);
 
 const StatesPage = ({ resumeSession, goToPage }) => {
 	const classes = useStyles();
@@ -91,10 +111,22 @@ const StatesPage = ({ resumeSession, goToPage }) => {
 							<Typography>{`${statsRow.language}, ${statsRow.difficulty}`}</Typography>
 							<Typography>Score: {statsRow.score}</Typography>
 							<Typography>{statsRow.state}</Typography>
-							<Typography>
-								{statsRow.playedWords ? statsRow.playedWords.length : 0}/{statsRow.words.length} words
-								played
-							</Typography>
+							<div>
+								<div className={classes.progressInfoContainer}>
+									<Typography>
+										{statsRow.playedWords ? statsRow.playedWords.length : 0}/{statsRow.words.length}{' '}
+										words played
+									</Typography>
+									<Typography>
+										{`${Math.floor(statsRow.playedWords.length * 100 / statsRow.words.length)}%`}
+									</Typography>
+								</div>
+								<BorderLinearProgress
+									variant="determinate"
+									color="secondary"
+									value={statsRow.playedWords.length * 100 / statsRow.words.length}
+								/>
+							</div>
 							{statsRow.playedWords &&
 							statsRow.playedWords.length > 0 && <SessionWordsStats stats={statsRow.playedWords} />}
 							{statsRow.state === 'Uncompleted' && (
