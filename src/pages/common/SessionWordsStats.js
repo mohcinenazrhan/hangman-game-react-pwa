051b2b380d1755d00394f44257408ee14fc00842
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-08-03 18:53:43 +0100

    feat: restyle stats session words conteiner

--- src/pages/common/SessionWordsStats.js
+++ src/pages/common/SessionWordsStats.js
@@ -5,18 +5,16 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		width: '100%'
-	},
-	paper: {
-		margin: theme.spacing(1, 0),
 		width: '100%',
-		overflowX: 'auto'
+		margin: theme.spacing(1, 0),
+		overflowX: 'auto',
+		border: '1px solid #E0E0E0',
+		borderRadius: '5px'
 	},
 	tableCell: {
 		padding: '5px'
@@ -43,54 +41,52 @@ const SessionWordsStats = ({ stats }) => {
 
 	return (
 		<div className={classes.root}>
-			<Paper className={classes.paper}>
-				<Table className={classes.table} size="small">
-					<TableHead>
-						<TableRow>
-							<TableCell>Word</TableCell>
-							<TableCell>State</TableCell>
-							<TableCell align="right">Points</TableCell>
-							<TableCell align="right">Misses</TableCell>
+			<Table className={classes.table} size="small">
+				<TableHead>
+					<TableRow>
+						<TableCell>Word</TableCell>
+						<TableCell>State</TableCell>
+						<TableCell align="right">Points</TableCell>
+						<TableCell align="right">Misses</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{stats.map((row, index) => (
+						<TableRow key={index}>
+							<TableCell
+								className={clsx(
+									classes.tableCell,
+									row.result === 'succeed' && classes.succeed,
+									row.result === 'failed' && classes.failed
+								)}
+							>
+								{row.word}
+							</TableCell>
+							<TableCell className={classes.tableCell}>
+								{row.wordState.map((item, i) => {
+									return (
+										<span
+											key={i}
+											className={clsx(
+												item.state === 'found' && classes.letterFound,
+												item.state === 'show' && classes.letterShow
+											)}
+										>
+											{item.letter}
+										</span>
+									);
+								})}
+							</TableCell>
+							<TableCell className={classes.tableCell} align="right">
+								{`${row.score}/${row.pointsToGain}`}
+							</TableCell>
+							<TableCell className={classes.tableCell} align="right">
+								{`${row.misses}/${row.wrongGuessAllowed}`}
+							</TableCell>
 						</TableRow>
-					</TableHead>
-					<TableBody>
-						{stats.map((row, index) => (
-							<TableRow key={index}>
-								<TableCell
-									className={clsx(
-										classes.tableCell,
-										row.result === 'succeed' && classes.succeed,
-										row.result === 'failed' && classes.failed
-									)}
-								>
-									{row.word}
-								</TableCell>
-								<TableCell className={classes.tableCell}>
-									{row.wordState.map((item, i) => {
-										return (
-											<span
-												key={i}
-												className={clsx(
-													item.state === 'found' && classes.letterFound,
-													item.state === 'show' && classes.letterShow
-												)}
-											>
-												{item.letter}
-											</span>
-										);
-									})}
-								</TableCell>
-								<TableCell className={classes.tableCell} align="right">
-									{`${row.score}/${row.pointsToGain}`}
-								</TableCell>
-								<TableCell className={classes.tableCell} align="right">
-									{`${row.misses}/${row.wrongGuessAllowed}`}
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</Paper>
+					))}
+				</TableBody>
+			</Table>
 		</div>
 	);
 };
