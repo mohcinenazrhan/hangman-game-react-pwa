dd83c12f67d072c511c26efba87d0b8514b3845f
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-08-09 21:00:59 +0100

    feat: creat SpinnerLoader component to make it reusable

---
+++ src/pages/common/SpinnerLoader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CircularProgress } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+const useStyles = makeStyles((theme) => ({
+	loaderFullPageCenter: {
+		position: 'absolute',
+		left: '50%',
+		top: '50%',
+		transform: 'translate(-50%, -50%)'
+	},
+	loaderNormal: {
+		margin: '24px auto'
+	}
+}));
+
+const SpinnerLoader = ({ fullPageCenter = false, message = null }) => {
+	const classes = useStyles();
+
+	return (
+		<div className={clsx(fullPageCenter ? classes.loaderFullPageCenter : classes.loaderNormal)}>
+			{message !== null && <p>{message}</p>}
+			<CircularProgress />
+		</div>
+	);
+};
+
+export default SpinnerLoader;
